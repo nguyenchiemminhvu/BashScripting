8cec5672b36b21a3b60bacbc016fccc5c1d7cb49
vu3.nguyen <vu3.nguyen@lge.com>
2024-08-28 16:49:03 +0900

    update

--- basic_scripts/parameters.sh
+++ basic_scripts/parameters.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+echo "script file name: $0"
+echo "number of params: $#"
+echo "param 1: $1"
+echo "param 2: $2"
+echo "all param as array: $@"
+echo "all param as string $*"
+IFS=','
+echo "all param as string $*"
