afabdc85eebe0f0fbf6461edca9732a9d70e7b56
vu3.nguyen <vu3.nguyen@lge.com>
2024-08-29 17:26:16 +0900

    update conditional

--- basic_scripts/conditional.sh
+++ basic_scripts/conditional.sh
@@ -48,3 +48,27 @@ then
 fi
 
 [[ $0 == *.sh ]] && echo "$0 is a bash script"
+
+rm temp
+if [[ $? != 0 ]]
+then
+    echo "rm temp command failed"
+fi
+
+s="string"
+[[ $s1 == "this is $s 1" ]] && echo "match substring"
+
+[[ -e ${PWD}/$0 ]] && echo "$0 exists"
+[[ -f ${PWD}/$0 ]] && echo "$0 is a regular file"
+[[ -d ${PWD} ]] && echo "${PWD} is a directory"
+[[ -s ${PWD}/$0 ]] && echo "$0 is not empty"
+[[ -x ${PWD}/$0 ]] && echo "$0 is execurable"
+
+if ! [[ -e ${PWD}/temp ]] || ! [[ -s ${PWD}/temp ]]
+then
+    ! [[ -e ${PWD}/temp ]] && touch temp && echo "temp does not exist, create it"
+    sleep 1
+    ! [[ -s ${PWD}/temp ]] && echo "make it non-empty" > ${PWD}/temp && echo "make it non-empty"
+    sleep 1
+    rm ${PWD}/temp && echo "remove temp file"
+fi
