78155466371761da93aeeffe7f5c06e0f093f8ac
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2024-08-29 16:08:12 +0000

    update array

--- basic_scripts/data_array.sh
+++ basic_scripts/data_array.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
 
+declare -a mics_arr
+misc_arr=(1 2 3 "hey" [10]=10)
+echo ${misc_arr[@]}
+
+declare -a file_arr
+file_arr=(*.sh)
+for i in $(seq ${#file_arr[@]})
+do
+    echo ${file_arr[$i]}
+done
+
+declare -A person
+person[name]="NCMV"
+person[job]="Programmer"
+echo ${person[name]} ${person[job]}
+
+for key in ${!person[@]}
+do
+    echo ${person[$key]}
+done
+
+[[ ${person[@]} =~ "Programmer" ]] && echo "We have a programmer here"
