0955006d24e5c2652ef2143918881c2309fd9184
vu3.nguyen <vu3.nguyen@lge.com>
2024-08-29 17:30:05 +0900

    bash loop

--- basic_scripts/loops.sh
+++ basic_scripts/loops.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 
+s=""
+for i in {1..5}
+do
+    s+="$i "
+done
+echo $s
+
+echo 
+
+s=""
+counter=1
+while [[ counter -le 10 ]]
+do
+    s+="$counter "
+    (( counter++ ))
+done
+echo $s
