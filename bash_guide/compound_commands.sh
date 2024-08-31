0fa31e6e8922e039002a42a18b6cebef3fbe4d62
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2024-08-31 16:31:08 +0000

    compound commands

--- bash_guide/compound_commands.sh
+++ bash_guide/compound_commands.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 
+PROCESS_NAME="PARENT"
+
+# execute commands in a child process
+(
+    PROCESS_NAME="CHILD"
+    ping -c 1 localhost > temp.txt
+    echo "child process is done"
+)
+
+echo "Current process is $PROCESS_NAME"
+cat temp.txt
+rm temp.txt
+
+# execute commands in the current process
+{
+    echo $SECONDS
+    sleep 1
+    echo $SECONDS
+}
