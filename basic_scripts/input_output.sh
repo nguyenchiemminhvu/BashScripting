cb514d36c692fd0f346b5db6e175db431bf7fefb
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2024-08-31 13:17:58 +0000

    update input output

--- basic_scripts/input_output.sh
+++ basic_scripts/input_output.sh
@@ -4,9 +4,39 @@
 # Standard Output (stdout): File Descriptor 1
 # Standard Error (stderr): File Descriptor 2
 
+usage() {
+    cat <<EOF
+usage: ./input_output.sh
+No parameter is required
+Firstly, the script will ask you to enter a number.
+If you enter something not a number, a complain will output.
+This usage print technique is called heredocs.
+It allows multi-line input to the command.
+EOF
+}
+
+usage
+
 read -p "Input your number: " num
 num_reg='^[0-9]+$$'
 if ! [[ $num =~ $num_reg ]]
 then
     echo "What is that? I told you to input a number!!!"
 fi
+
+echo "This regular text is redirected to a file" > temp.txt
+cat temp.txt
+
+ls non_existing_file.txt 2> temp.txt
+cat temp.txt
+
+cat temp.txt > /dev/null 2>&1 # output nothing
+
+cat < temp.txt # input from a file instead of stdin
+
+rm temp.txt
+
+grep "string" <<<"This is a multiline string
+it is called a herestring
+you can enter whatever you want here
+and this string will become the input for the grep command above"
