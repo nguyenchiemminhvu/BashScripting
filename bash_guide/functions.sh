b0a306889151891f87afce430f6410035c275f38
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2024-08-31 16:44:02 +0000

    update

--- bash_guide/functions.sh
+++ bash_guide/functions.sh
@@ -1,2 +1,31 @@
 #!/bin/bash
 
+sum()
+{
+    local n1=$1
+    local n2=$2
+    echo "$n1 + $n1 = $(($n1 + $n1))"
+}
+
+sub()
+{
+    echo "$1 - $2 = $(($1 - $2))"
+}
+
+mul()
+{
+    echo "$1 * $2 = $(($1 * $2))"
+}
+
+div()
+{
+    echo "$1 / $2 = $(($1 / $2))"
+}
+
+if [[ $# -ge 2 ]]
+then
+    sum $1 $2
+    sub $1 $2
+    mul $1 $2
+    [[ $2 -ne 0 ]] && div $1 $2 || echo "Can not divide by 0"
+fi
