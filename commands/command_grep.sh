3ff8cbb35c5efd0e380071d86a3dccec21cf5d54
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2024-09-01 13:44:54 +0000

    update grep

--- commands/command_grep.sh
+++ commands/command_grep.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 
+# grep [options] pattern [file...]
+
+grep "bash" $0
+
+grep -v "bash" $0 # invert, print non-match
+
+grep -r "bash" ./../ # recursive search
+
+grep -n "bash" $0 # show number of match line
+
+grep -c "bash" $0 # count number of match line
+
+grep -l "bash" *.sh # all files contain the match
+
+ps -aux | grep bash
+
+grep -E ".*bash$" *.sh # match regex pattern
