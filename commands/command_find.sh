cfbfe21ce5f662f27b0aa4a7e2318c2ede853900
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2024-09-01 13:35:42 +0000

    update find command

--- commands/command_find.sh
+++ commands/command_find.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+# find [path] [options] [expression]
+
+find . -name "command_find.sh"
+find . -iname "Command_Find.sh"
+
+find . -type f | grep find
+
+find .. -maxdepth 2 -type f -name "command_find.sh"
+
+find .. -type d -name "commands" -exec find {} -name "command_find.sh" \;
+
+find . -type f -empty -exec echo "Empty files: {}" \;
